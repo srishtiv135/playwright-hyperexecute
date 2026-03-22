bbbb5fdc2e194340e31529bfe9ea1cb821391cde
srishtiv135 <sri.vijayvargiya@gmail.com>
2026-03-22 16:43:46 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,21 @@
 import pytest
 from playwright.sync_api import sync_playwright
 import os
-import json
-import urllib.parse
 
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-
-        lt_username = os.getenv("LT_USERNAME", "")
-        lt_access_key = os.getenv("LT_ACCESS_KEY", "")
-
-        capabilities = {
-            "browserName": "Chrome",
-            "browserVersion": "latest",
-            "LT:Options": {
-                "username": lt_username,
-                "accessKey": lt_access_key,
-                "platformName": "Windows 10",
-                "project": "TestMu Playwright Assignment",
-                "build": "Playwright HyperExecute Build",
-                "name": "TestMu AI Playground Tests",
-                "network": True,
-                "video": True,
-                "screenshot": True,
-                "console": True,
-                "w3c": True
-            }
-        }
-
-        browser = p.chromium.connect(
-            f"wss://cdp.lambdatest.com/playwright?capabilities="
-            f"{urllib.parse.quote(json.dumps(capabilities))}"
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(
+            record_video_dir="test-results/videos/",
         )
-
-        context = browser.new_context()
+        context.tracing.start(
+            screenshots=True,
+            snapshots=True,
+            sources=True
+        )
         page = context.new_page()
         yield page
+        context.tracing.stop(path="test-results/trace.zip")
         context.close()
         browser.close()
