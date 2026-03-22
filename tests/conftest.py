5e027f263c1725b0d2551528b8924f865567ac1a
srishtiv135 <sri.vijayvargiya@gmail.com>
2026-03-22 13:28:08 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from playwright.sync_api import sync_playwright
+import os
 
 @pytest.fixture
 def page():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+        context = browser.new_context(
+            record_video_dir="test-results/videos/",
+        )
+        context.tracing.start(
+            screenshots=True,
+            snapshots=True,
+            sources=True
+        )
+        page = context.new_page()
         yield page
-        page.close()
+        context.tracing.stop(path="test-results/trace.zip")
+        context.close()
         browser.close()
